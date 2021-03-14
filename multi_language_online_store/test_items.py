c49e80cdbc4f7beac7c7b645ec88aa44b0f061fb
valeshin <v.aleshin92@gmail.com>
2021-03-14 04:51:27 +0300

    corrected the assertion in the test function

--- multi_language_online_store/test_items.py
+++ multi_language_online_store/test_items.py
@@ -4,4 +4,4 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_presence_of_add_to_basket_button(browser):
     browser.get(link)
     add_to_basket_button = browser.find_elements_by_class_name("btn-add-to-basket")
-    assert add_to_basket_button != 0, "Add to basket button is missing on the page!!!"
+    assert len(add_to_basket_button) != 0, "Add to basket button is missing on the page!!!"
